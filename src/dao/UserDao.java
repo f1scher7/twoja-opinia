9629c4cebfe1700add54a9e81d56b13ba96bf3bb
f1scher7 <maksym.szylo@gmail.com>
2023-11-22 21:11:15 +0100

    Add dao classes for communication with data base

---
+++ src/dao/UserDao.java
@@ -0,0 +1,9 @@
+//UserDao.java: Klasa odpowiedzialna za operacje związane z dostępem do danych użytkowników w bazie danych.
+// Może zawierać metody takie jak dodawanie nowego użytkownika, pobieranie informacji o użytkowniku
+// na podstawie identyfikatora, aktualizowanie danych użytkownika, itp.
+
+package dao;
+
+public class UserDao {
+
+}
