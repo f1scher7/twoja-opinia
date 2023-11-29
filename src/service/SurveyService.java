fc22c69f65881fdbac527ab7104a551f723da022
f1scher7 <maksym.szylo@gmail.com>
2023-11-29 10:47:40 +0100

    Add service classes for business logic

---
+++ src/service/SurveyService.java
@@ -0,0 +1,9 @@
+//SurveyService.java: Klasa serwisowa odpowiedzialna za operacje związane z ankietami.
+// Wśród jej metod mogą być funkcje takie jak tworzenie nowej ankiety,
+// pobieranie listy ankiet, zbieranie wyników ankiet, itp.
+
+package service;
+
+public class SurveyService {
+
+}
---
+++ src/service/UserService.java
@@ -0,0 +1,9 @@
+//UserService.java: Klasa serwisowa obsługująca operacje związane z użytkownikami.
+// Może zawierać metody takie jak rejestracja nowego użytkownika, uwierzytelnianie,
+// pobieranie informacji o użytkowniku, zarządzanie sesją użytkownika, itp.
+
+package service;
+
+public class UserService {
+    
+}
