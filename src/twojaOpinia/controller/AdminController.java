f64889eed216c6fcdd4151abeb30ac6adaf9bbb8
Sebrinn <sebastian_k2@interia.pl>
2023-12-01 17:54:39 +0100

    logowanie

--- src/twojaOpinia/controller/AdminController.java
+++ src/twojaOpinia/controller/AdminController.java
@@ -3,11 +3,16 @@ package twojaOpinia.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import twojaOpinia.dao.UserDao;
+import twojaOpinia.model.User;
 
 public class AdminController {
 
     @FXML
     private TextField userNameField;
+    
+    @FXML
+    private TextField userPasswordField;
 
     @FXML
     private TextField surveyNameField;
@@ -21,7 +26,14 @@ public class AdminController {
     @FXML
     private void handleCreateUser() {
         String username = userNameField.getText();
+        String password = userPasswordField.getText();
         System.out.println("Creating user: " + username);
+        
+        User user = new User(username, password, false);      
+        UserDao userDao = new UserDao();
+        
+        userDao.saveUser(user);
+        
     }
 
     @FXML
