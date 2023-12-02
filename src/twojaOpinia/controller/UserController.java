4bbab878946f505f9f72e4a5ff682d9126d632a1
f1scher7 <maksym.szylo@gmail.com>
2023-12-02 22:07:05 +0100

    Add basic window for user which logged in

--- src/twojaOpinia/controller/UserController.java
+++ src/twojaOpinia/controller/UserController.java
@@ -1,4 +1,30 @@
 package twojaOpinia.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import twojaOpinia.dao.UserDao;
+import twojaOpinia.model.User;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class UserController {
+    @FXML
+    private Button logout;
+    @FXML
+    private void logout() {
+        try {
+            Parent login = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/twojaOpinia/view/login/LoginView.fxml")));
+            Scene scene = new Scene(login);
+            Stage stage = (Stage) logout.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
