23c3fb0dd3b6f3e3bf2b85ffee674d3ca45d61c6
f1scher7 <maksym.szylo@gmail.com>
2023-12-02 22:08:55 +0100

    Some fixes

--- src/twojaOpinia/dao/UserDao.java
+++ src/twojaOpinia/dao/UserDao.java
@@ -1,61 +1,44 @@
-//UserDao.java: Klasa odpowiedzialna za operacje związane z dostępem do danych użytkowników w bazie danych.
-// Może zawierać metody takie jak dodawanie nowego użytkownika, pobieranie informacji o użytkowniku
-// na podstawie identyfikatora, aktualizowanie danych użytkownika, itp.
-
 package twojaOpinia.dao;
 
-import java.sql.*;
 import twojaOpinia.model.User;
 import twojaOpinia.util.DataBaseUtil;
+import twojaOpinia.util.SHA256;
+
+import java.sql.*;
 
 public class UserDao {
-	
- 	private Connection connection;
- 	private Statement statement;
- 	private String query;
+	private Connection connection;
+	private Statement statement;
+	private String query;
 
 	public void saveUser(User user) {
- 		query = "INSERT INTO `users` (`login`, `password`, `admin`) VALUES ('" + user.getLogin() + "', '" + user.getPassword() + "', '" + 1 + "');";
- 		try
- 		{
- 			this.connection = DataBaseUtil.connect();
- 			this.statement = connection.createStatement();
- 			this.statement.executeUpdate(query);
-            this.statement.close();
-            this.connection.close();        
- 		} 
- 		catch (SQLException e)
- 		{
- 			// TODO Auto-generated catch block
- 			System.out.println("Użytkownik o tym loginie już istnieje");
- 		}
- 	}
-	
-	public User findUser(String login)
-	{
+		String hashedPassword = SHA256.toSHA256(user.getPassword());
+		query = "INSERT INTO `users` (`login`, `password`, `admin`) VALUES ('" + user.getLogin() + "', '" + hashedPassword + "', '" + (user.isAdmin() ? 1 : 0) + "');";
+		try {
+			this.connection = DataBaseUtil.connect();
+			this.statement = connection.createStatement();
+			this.statement.executeUpdate(query);
+			this.statement.close();
+			this.connection.close();
+		} catch (SQLException e) {
+			System.out.println("Użytkownik o tym loginie już istnieje");
+		}
+	}
+
+	public User findUser(String login) {
 		User user = null;
 		query = "SELECT * FROM `users` WHERE users.login = '" + login + "'";
- 		try
- 		{
- 			this.connection = DataBaseUtil.connect();
- 			this.statement = connection.createStatement();
- 			
- 			ResultSet result = this.statement.executeQuery(query);
- 			
- 			result.next();
- 			user = new User(login, result.getString("password"), result.getBoolean("admin"));
- 			
-            this.statement.close();
-            this.connection.close();        
- 		} 
- 		catch (SQLException e)
- 		{
- 			// TODO Auto-generated catch block
- 			//e.printStackTrace();
- 			System.out.println("Nie ma takiego uzytkownika");
- 		}
- 		return user;
-		
+		try {
+			this.connection = DataBaseUtil.connect();
+			this.statement = connection.createStatement();
+			ResultSet result = this.statement.executeQuery(query);
+			result.next();
+			user = new User(login, result.getString("password"), result.getBoolean("admin"));
+			this.statement.close();
+			this.connection.close();
+		} catch (SQLException e) {
+			System.out.println("Nie ma takiego użytkownika");
+		}
+		return user;
 	}
 }
-
