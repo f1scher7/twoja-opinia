bfa1657f2554ef8420d784606b557533af249afa
Sebrinn <sebastian_k2@interia.pl>
2023-12-03 12:55:51 +0100

    gitignore

--- src/twojaOpinia/model/Survey.java
+++ src/twojaOpinia/model/Survey.java
@@ -3,6 +3,37 @@
 
 package twojaOpinia.model;
 
-public class Survey {
+import java.util.Vector;
 
+public class Survey {
+	
+	private int id;
+	private User author;
+	private String title;
+	private String description;
+	private Vector <String> answers;
+	
+	private Vector <Vote> votes;
+	
+	
+	Survey()
+	{
+		this.id = -1;
+		this.author = null;
+		this.title = "";
+		this.description = "";
+		this.answers = new Vector<String>();
+		this.votes = new Vector<Vote>();
+	}
+	
+	public Survey(int id, User author, String title, String description)
+	{
+		this.id = id;
+		this.author = author;
+		this.title = title;
+		this.description = description;
+		this.answers = new Vector<String>();
+		this.votes = new Vector<Vote>();
+	}
+	
 }
