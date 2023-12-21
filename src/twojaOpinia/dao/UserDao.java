872231dea924c808fbca1f7dd36559395e73ccdd
f1scher7 <maksym.szylo@gmail.com>
2023-12-21 20:57:56 +0100

    Added deleteUserByLogin method and modified insert method

--- src/twojaOpinia/dao/UserDao.java
+++ src/twojaOpinia/dao/UserDao.java
@@ -5,7 +5,6 @@ import twojaOpinia.util.DataBaseUtil;
 import static twojaOpinia.util.SaltUtil.generateSalt;
 import twojaOpinia.util.SHA256;
 
-import java.io.IOException;
 import java.sql.*;
 
 public class UserDao implements InterfaceDAO<User, String>{
@@ -19,26 +18,32 @@ public class UserDao implements InterfaceDAO<User, String>{
 		String salt = generateSalt();
 		user.setSalt(salt);
 		String hashedPassword = SHA256.toSHA256(user.getPassword() + salt);
-		String query = "INSERT INTO `users` (`login`, `password`, `salt`, `admin`) VALUES (?, ?, ?, ?)";
+		String query = "INSERT INTO `users` (`name`, `surname`, `email`, `birthday`, `country`, `city`, `login`, `password`, `salt`, `admin`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		try (Connection connection = DataBaseUtil.connect(); PreparedStatement preparedStatement = connection.prepareStatement(query)){
-			preparedStatement.setString(1, user.getLogin());
-			preparedStatement.setString(2, hashedPassword);
-			preparedStatement.setString(3, salt);
-			preparedStatement.setBoolean(4, user.isAdmin());
+			preparedStatement.setString(1, user.getName());
+			preparedStatement.setString(2, user.getSurname());
+			preparedStatement.setString(3, user.getEmail());
+			preparedStatement.setString(4, user.getBirthday());
+			preparedStatement.setString(5, user.getCountry());
+			preparedStatement.setString(6, user.getCity());
+			preparedStatement.setString(7, user.getLogin());
+			preparedStatement.setString(8, hashedPassword);
+			preparedStatement.setString(9, salt);
+			preparedStatement.setBoolean(10, user.isAdmin());
 			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println("Użytkownik o tym loginie już istnieje");
 		}
 	}
 
-	public User getByLogin(String login) {
+	public User getUserDataByLogin(String login) {
 		User user = null;
 		String query = "SELECT * FROM `users` WHERE users.login = ?";
 		try (Connection connection = DataBaseUtil.connect(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 			preparedStatement.setString(1, login);
 			try (ResultSet resultSet = preparedStatement.executeQuery()) {
 				if (resultSet.next()) {
-					user = new User(login, resultSet.getString("password"), resultSet.getString("salt"), resultSet.getBoolean("admin"));
+					user = new User(resultSet.getString("name"), resultSet.getString("surname"), resultSet.getString("email"), resultSet.getString("birthday"), resultSet.getString("country"), resultSet.getString("city"), login, resultSet.getString("password"), resultSet.getString("salt"), resultSet.getBoolean("admin"));
 				}
 			}
 		} catch (SQLException e) {
@@ -63,8 +68,19 @@ public class UserDao implements InterfaceDAO<User, String>{
 		return res;
 	}
 
-	@Override
-	public void delete(int id) {
-
+	public int deleteUserByLogin(String userLogin) {
+		String query = "DELETE FROM users WHERE login = ?";
+		int affectedRows = 0;
+		try (Connection connection = DataBaseUtil.connect(); PreparedStatement preparedStatement = connection.prepareStatement(query)){
+			preparedStatement.setString(1, userLogin);
+			affectedRows = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("Błąd podczas usuwania użytkownika");
+			e.printStackTrace();
+		}
+		return affectedRows;
 	}
+
+	@Override
+	public void deleteByID(int id) {}
 }
